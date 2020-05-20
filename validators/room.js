d7079e924634947e13e7a25530b97525b2164e5f
Srinivas <nandusrinivas33.cse@gmail.com>
2020-05-20 21:06:21 +0530

    update room validators with nesessary fields

--- validators/room.js
+++ validators/room.js
@@ -4,10 +4,14 @@ const { Joi } = require('express-validation')
 
 const roomValidator = {
   body: Joi.object({
+    id: Joi.string().pattern(new RegExp('^[0-9a-fA-F]{24}$')),
     name: Joi.string(),
     owner: Joi.string().pattern(new RegExp('^[0-9a-fA-F]{24}$')),
     code: Joi.string(),
+    language: Joi.string().valid('python', 'javascript'),
     private: Joi.bool(),
+    createdAt: Joi.date(),
+    updatedAt: Joi.date(),
   }),
 }
 
