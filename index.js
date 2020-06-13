b9df723de9be81949a6ff2d3bfb0e39687df4b01
Srinivas <nandusrinivas33.cse@gmail.com>
2020-06-13 18:09:04 +0530

    add socketIO handlers to sync code between clients

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const logger = require('./utils/logger')
 const config = require('./utils/config')
 const http = require('http')
 const app = require('./app')
+const socketIO = require('socket.io')
 
 const server = http.createServer(app)
+
+// socketIO handlers
+const sockets = socketIO(server).of('/sockets/room')
+sockets.on('connection', (socket) => {
+  socket.on('join-room', (data) => {
+    socket.join(data.roomId)
+  })
+  socket.on('code-edited', (data) => {
+    socket.to(data.roomId).emit('code-edited', data)
+  })
+})
 
 server.listen(config.PORT, () => {
   logger.info(`Server is running on port ${config.PORT}`)
