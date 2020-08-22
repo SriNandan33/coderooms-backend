778d4113a4e7293d51f6a879d1d8a220327d5287
Srinivas <nandusrinivas33.cse@gmail.com>
2020-08-22 20:46:07 +0530

    add handlers to new user connected, disconnect events

--- index.js
+++ index.js
@@ -12,12 +12,20 @@ sockets.on('connection', (socket) => {
   socket.on('join-room', (data) => {
     socket.join(data.roomId)
     socket.to(data.roomId).emit('user-joined', data)
+
+    socket.on('disconnect', () => {
+      sockets.to(data.roomId).emit('user-disconnected')
+    })
   })
   socket.on('code-edited', (data) => {
     socket.to(data.roomId).emit('code-edited', data)
   })
   socket.on('drawing', (data) => {
     socket.to(data.roomId).emit('drawing', data)
+  })
+
+  socket.on('user-video-connected', (data) => {
+    socket.to(data.roomId).emit('user-video-connected', data)
   })
 })
 
